ab92377ffb10bedc2dad055cbc0e6dfd44764bf2
etklam <mail@kwunlam.com>
2021-12-03 01:35:13 +0800

    basic insert function

--- btree.py
+++ btree.py
@@ -7,10 +7,27 @@ class Node (object):
         self.leaf = True
 
 class BTree (object):
-    def __init__(self, maxLength):
-        maxLength = 5
+    def __init__(self, maxLength = 5):
         self.root = Node(maxLength)
 
 #    def insert(key, value):
 #        # check is it a empty node
 #        if not self.keys:
+    def insert(self, key, value):
+        # The very first insert
+        if self.keys == []:
+            self.keys.append(key)
+            self.value.append([value])
+            return
+
+        for i, element in enumerate(self.keys):
+            if key <= element:
+                self.keys = self.keys[:i] + [key] + self.keys[i:]
+                self.values = self.values[:i] + [[value]] + self.values[i:]
+                break
+            
+            else:
+                self.keys.append(key)
+                self.values.append([value])
+
+
