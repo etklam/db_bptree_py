83899c134c131014a29085f8e9c2b7f7cf89019e
etklam <mail@kwunlam.com>
2021-12-03 02:44:32 +0800

    update print and fullcheck

--- main.py
+++ main.py
@@ -9,32 +9,43 @@ class Node (object):
         if self.keys == []:
             self.keys.append(key)
             self.values.append([value])
-            return
+            return self
 
         for i, element in enumerate(self.keys):
             if key <= element:
                 #[:i] means array before i; [i:] means array after i
                 self.keys = self.keys[:i] + [key] + self.keys[i:]
                 self.values = self.values[:i] + [[value]] + self.values[i:]
-                print("<=",self.keys)
+                # print("<=",self.keys)
                 break
-            elif i == len(self.keys)-1:
+            elif len(self.keys)-1 == i:
                 self.keys.append(key)
                 self.values.append([value])
-                print("tail",self.keys)
+                # print("tail",self.keys)
                 break
+        return self
+    
+    def listAllkeys(self):
+        print(self.keys)
+
+    def leafFull():
+        if len(keys) >= maxLength:
+            return True
+        return False
+
 class BTree (object):
     def __init__(self, maxLength ):
         self.root = Node(maxLength)
 
     def insert(self, node, key):
-        root.insert(node, key, value)
+        self.root.insert(node, key, value)
+        self.root.listAllkeys()
     
     def search(self, node, key):
         for i, element in enumerate(node.keys):
             if key < element:
-                return node.values[i], i
-        return node.values[i + 1], i + 1
+                return i
+        return i + 1
 
     # def insert(self, key, value):
     #     # scanning from root
