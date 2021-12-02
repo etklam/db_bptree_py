2e51dcafd3632c53e9b5cf900afa7c318f3878ed
etklam <mail@kwunlam.com>
2021-12-03 02:13:35 +0800

    test the insert and fix the file reader

--- main.py
+++ main.py
@@ -9,30 +9,19 @@ class Node (object):
         if self.keys == []:
             self.keys.append(key)
             self.values.append([value])
-            print("node")
-            print(self.keys)
             return
 
-
-    #    def insert(key, value):
-    #        # check is it a empty node
-    #        if not self.keys:
-    # def insert(self, key, value):
-    #     # The very first insert
-    #     if self.keys == []:
-    #         self.keys.append(key)
-    #         self.value.append([value])
-    #         return
-
-    #     for i, element in enumerate(self.keys):
-    #         if key <= element:
-    #             self.keys = self.keys[:i] + [key] + self.keys[i:]
-    #             self.values = self.values[:i] + [[value]] + self.values[i:]
-    #             break
-            
-    #         else:
-    #             self.keys.append(key)
-    #             self.values.append([value])
+        for i, element in enumerate(self.keys):
+            if key >= element:
+                #[:i] means array before i; [i:] means array after i
+                self.keys = self.keys[:i] + [key] + self.keys[i:]
+                self.values = self.values[:i] + [[value]] + self.values[i:]
+                print(self.keys)
+                break
+            else:
+                self.keys.append(key)
+                self.values.append([value])
+                break
 
 class BTree (object):
     def __init__(self, maxLength ):
@@ -41,18 +30,19 @@ class BTree (object):
     def insert(self, node, key):
         root.insert(node, key, value)
     
-    # def search(self, node, key):
-    #     for i, element in enumerate(node.keys):
-    #         if key < element:
-    #             return node.values[i], i
-    #     return node.values[i + 1], i + 1
+    def search(self, node, key):
+        for i, element in enumerate(node.keys):
+            if key < element:
+                return node.values[i], i
+        return node.values[i + 1], i + 1
 
     # def insert(self, key, value):
     #     # scanning from root
     #     parent = None
     #     child = self.root
 class main():
+    #f = open("./debug.txt", "r")
     f = open("./file.txt", "r")
     tree = BTree(5)
     for x in f:
-        tree.root.insert(x, 0)
+        tree.root.insert(x.rstrip(), 0)
